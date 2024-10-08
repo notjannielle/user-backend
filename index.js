d2336276424f16fa9a847015cde8f8f274f70e84
notjannielle <2020-107544@rtu.edu.ph>
2024-10-08 18:29:50 +0800

    TEST AGAIN

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ mongoose.connect(process.env.MONGO_URI, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: {
-    version: '1', // Corrected: Specify the server API version as a string
+    version: ServerApiVersion.v1,
     strict: true, // Enable strict mode
     deprecationErrors: true, // Enable deprecation errors
   },
